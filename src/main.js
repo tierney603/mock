bdbfd183ffa695d4ca33d19c4426801e15a55fb4
tiereny <1336260814@qq.com>
2022-01-13 18:25:58 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 Vue.config.productionTip = false
+
+process.env.VUE_APP_MOCK === 'true' && require('../mock')
+
 
 new Vue({
   router,
